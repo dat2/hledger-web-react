d450360a3f1d8b7d4432e9e60147d20203fdd4c2
Nicholas Dujay <nickdujay@gmail.com>
2017-12-04 21:34:30 -0500

    add local forage caching

--- src/Root/Root.saga.js
+++ src/Root/Root.saga.js
@@ -1,4 +1,6 @@
 import { all, call, put, takeEvery } from 'redux-saga/effects';
+import localforage from 'localforage';
+
 import Actions from './Root.actions';
 
 export default function* rootSaga() {
@@ -20,9 +22,20 @@ function* fetchAccountNames() {
 
 function* fetchTransactions() {
   try {
+    // fetch from cache
+    const cached = yield call(() => localforage.getItem('transactions'));
+    if(cached) {
+      yield put(Actions.fetchTransactionsSuccess({ data: cached, isCached: true }));
+    }
+
+    // just in case things are updated, we'll just dispatch it twice :)
     const response = yield call(fetch, '/api/v1/transactions');
-    const json = yield call(() => response.json());
-    yield put(Actions.fetchTransactionsSuccess(json));
+    const data = yield call(() => response.json());
+    yield put(Actions.fetchTransactionsSuccess({ data, isCached: false }));
+
+    // cache it
+    yield call(() => localforage.setItem('transactions', data));
+
   } catch (e) {
     yield put(Actions.fetchTransactionsFailed(e));
   }
