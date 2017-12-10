da84145d5f48a56bc3f0209eca8e8140fda4dc70
Alberto Leal <mailforalberto@gmail.com>
2017-12-10 15:43:59 -0500

    RootProps

--- src/Root/component.js
+++ src/Root/component.js
@@ -12,14 +12,14 @@ import NotFound from '../NotFound';
 import Dashboard from '../Dashboard';
 import Accounts from '../Accounts';
 
-type Props = {
+type RootProps = {
   // TODO: change this type
   accounts: any,
   fetchAccounts: () => void,
   loadTransactions: () => void
 };
 
-class Root extends Component<Props> {
+class Root extends Component<RootProps> {
   static propTypes = {
     accounts: PropTypes.object.isRequired,
     fetchAccounts: PropTypes.func.isRequired,
